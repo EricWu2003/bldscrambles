fdb6dea88c0dca06a2d89154b94c90624e2373be
Eric Wu <eric.dianhao.wu@gmail.com>
2022-09-02 10:29:15 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Canvas from './Canvas';
+
+const Cube = require('cubejs');
 
 function App() {
+  const [cubeState, setCubeState] = React.useState("");
+
+  function handleClick() {
+    const cube = new Cube();
+  
+    let scramble = generateRandomScramble();
+  
+    cube.move(scramble);
+  
+    console.log(`your scramble is: ${scramble}`);
+  
+    setCubeState(cube.asString());
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => handleClick()}>Click me</button>
+        <Canvas cubeString={cubeState} width={300} height={300}></Canvas>
       </header>
     </div>
   );
 }
+
+
+
+function generateRandomScramble() {
+  // we will generate a scramble between 22 and 25 moves
+  let moves = ["R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"];
+  let scramble = "";
+  let numMoves = getRndInteger(22, 25);
+  let forbidden = "*";
+  for (let i = 0; i < numMoves; i ++) {
+    let nextMove = moves[getRndInteger(0, moves.length - 1)];
+    while (nextMove.includes(forbidden)) {
+      nextMove = moves[getRndInteger(0, moves.length - 1)];
+    }
+
+    forbidden = nextMove[0];
+    scramble += nextMove + " ";
+  }
+  return scramble;
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 
 export default App;
---
+++ src/Canvas.js
@@ -0,0 +1,55 @@
+import React, {useRef, useEffect} from 'react'
+
+const SIZE = 20;
+
+const colors = {
+    U: '#FFFFFF',
+    D: '#FFFF00',
+    L: '#FFA500',
+    R: '#FF0000',
+    F: '#00FF00',
+    B: '#0000FF',
+}
+
+const Canvas = props => {
+    const { cubeString } = props;
+
+  
+    const canvasRef = useRef(null)
+    
+    
+    
+    useEffect(() => {
+        const draw = ctx => {
+            drawSquare(ctx, cubeString, 3*SIZE, 0);
+            drawSquare(ctx, cubeString.slice(9), 6*SIZE, 3*SIZE);
+            drawSquare(ctx, cubeString.slice(18), 3*SIZE, 3*SIZE);
+            drawSquare(ctx, cubeString.slice(27), 3*SIZE, 6*SIZE);
+            drawSquare(ctx, cubeString.slice(36), 0*SIZE, 3*SIZE);
+            drawSquare(ctx, cubeString.slice(45), 9*SIZE, 3*SIZE);
+        }
+
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        
+        //Our draw come here
+        draw(context)
+    }, [canvasRef, cubeString])
+    
+    return <canvas ref={canvasRef} {...props}/>
+}
+
+const drawSquare =  (ctx, square, x, y) => {
+    const positions = [[0,0], [1,0], [2,0], [0,1], [1,1], [2,1], [0,2], [1,2], [2,2]];
+
+    ctx.strokeStyle = "#000000";
+    for (let i = 0; i < 9; i ++) {
+        ctx.fillStyle = colors[square[i]];
+        ctx.fillRect(x + positions[i][0] * SIZE, y + positions[i][1] * SIZE, SIZE, SIZE);
+        ctx.strokeRect(x + positions[i][0] * SIZE, y + positions[i][1] * SIZE, SIZE, SIZE);
+    }
+
+}
+
+
+export default Canvas
