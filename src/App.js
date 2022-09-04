5a70a0d6f1bfe61d26e4da62d5098f23f933cb7c
Eric Wu <eric.dianhao.wu@gmail.com>
2022-09-04 19:33:33 -0400

    set default bg color

--- src/App.js
+++ src/App.js
@@ -3,34 +3,44 @@ import Canvas from './Canvas';
 import { CssBaseline, Container } from '@mui/material';
 import Header from './Header';
 import { generateRandomScramble } from './utils';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 
 
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: "#e4f0e2"
+    }
+  }
+});
 
 const App = () => {
   const [currentScramble, setCurrentScramble] = React.useState(generateRandomScramble());
 
 
   return (
-    <div className="App">
-      <CssBaseline></CssBaseline>
-      <header className="App-header">
-        <Header 
-          currentScramble={currentScramble}
-          setCurrentScramble={setCurrentScramble}
-        />
-      </header>
-
-      <main>
-        <Container maxWidth="md">
-          <Canvas 
-            currentScramble={currentScramble} 
-            width={300}
-            height={220}
+    <ThemeProvider theme={themeLight}>
+      <div className="App">
+        <CssBaseline></CssBaseline>
+        <header className="App-header">
+          <Header 
+            currentScramble={currentScramble}
+            setCurrentScramble={setCurrentScramble}
           />
-        </Container>
-      </main>
-    </div>
+        </header>
+
+        <main>
+          <Container maxWidth="md">
+            <Canvas 
+              currentScramble={currentScramble} 
+              width={300}
+              height={220}
+            />
+          </Container>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
